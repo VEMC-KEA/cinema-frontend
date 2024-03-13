9a2089f1f81972a565cca0e8522bdf381a937754
emilvn <emilvnielsen@hotmail.com>
2024-03-13 14:40:34 +0100

    some styling and some icons

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren } from "react";
 import { Link } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { LuLogIn, LuPopcorn } from "react-icons/lu";
 
 function PageLayout({ children }: PropsWithChildren) {
     return (
@@ -16,13 +17,43 @@ function PageLayout({ children }: PropsWithChildren) {
 function NavBar() {
     return (
         <nav className="fixed bg-white w-full top-0 h-20 items-center p-4 text-lg flex justify-between">
-            <div className="flex gap-3">
-                <Link to="/screenings">Forestillinger</Link>
-                <Link to="/movies">Film</Link>
-                <Link to="/cinemas">Biografer</Link>
-                <Link to="/reservations">Reservationer</Link>
+            <div className="flex gap-3 items-center">
+                <LuPopcorn className="text-4xl text-red-600" />
+                <Link
+                    className="font-semibold text-stone-700 hover:text-stone-400 transition-colors"
+                    to="/movies"
+                >
+                    Film
+                </Link>
+                <Link
+                    className="font-semibold text-stone-700 hover:text-stone-400 transition-colors"
+                    to="/cinemas"
+                >
+                    Biografer
+                </Link>
             </div>
-            <Link to="/login">Log ind</Link>
+            <div className="flex gap-3 items-center">
+                {/*TODO: Make available only when logged in*/}
+                <Link
+                    className="font-semibold text-stone-700 hover:text-stone-400 transition-colors"
+                    to="/screenings"
+                >
+                    Forestillinger
+                </Link>
+                <Link
+                    className="font-semibold text-stone-700 hover:text-stone-400 transition-colors"
+                    to="/reservations"
+                >
+                    Reservationer
+                </Link>
+                {/*TODO: Change to log out when logged in*/}
+                <Link
+                    className="font-semibold text-stone-700 hover:text-stone-400 transition-colors"
+                    to="/login"
+                >
+                    <LuLogIn className="text-3xl" />
+                </Link>
+            </div>
         </nav>
     );
 }
